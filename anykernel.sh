88fbbb6538806c334c519c4fd5b4826cadc641c7
Gedō Mazō <42801308+rianaryandi@users.noreply.github.com>
2023-09-09 17:48:17 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=A Kernel Compiler by @Gabuters
+kernel.string=A Kernel Compiler by RIAN ARYANDI
 do.devicecheck=1
 do.modules=0
 do.systemless=0
